9c587ee93e1c291568d1e603cc0a6a8fe9b8d550
CcccFz <ccccfz@163.com>
2023-08-03 01:59:27 +0800

    [geektime url convert] feat: open geektime

--- geektime-url-convert/background.js
+++ geektime-url-convert/background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener(function (tab) {
   chrome.tabs.create({
-    url: tab.url.replace("1/object-browser", "0")
+    url: 'http://localhost:9001/object-browser'
   });
 });
