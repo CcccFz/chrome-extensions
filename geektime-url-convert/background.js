f043437270fad1a6d3dee6b5d6e5fb68b96acf5d
CcccFz <ccccfz@163.com>
2023-11-13 23:37:23 +0800

    指定特定域名才能使用插件

--- geektime-url-convert/background.js
+++ geektime-url-convert/background.js
@@ -3,3 +3,27 @@ chrome.action.onClicked.addListener(function (tab) {
     url: 'http://localhost:9001'
   });
 });
+
+// 指定特定域名才能使用插件
+// const ALLOW_HOSTS = ['www.baidu.com', 'google.com'];
+
+// chrome.tabs.onActivated.addListener(async () => {
+//   const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+//   switchAction(tab);
+// });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   switchAction(tab);
+// });
+
+// function switchAction(tab) {
+//   if (!tab.url) {
+//     return;
+//   }
+
+//   if (ALLOW_HOSTS.includes(new URL(tab.url).host)) {
+//     chrome.action.enable();
+//   } else {
+//     chrome.action.disable();
+//   }
+// }
